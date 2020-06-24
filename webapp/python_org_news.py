dcb43b8774a1d1fb2f07a7801b5b93c438c9b480
sfkl-i <safa1320@gmail.com>
2020-06-25 00:57:17 +0300

    Добавлена поддержка БД Flask-SQLAlchemy

--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+
+from webapp.model import db, News
 
 def get_html(url):
     try:
@@ -21,11 +24,18 @@ def get_python_news():
             title = news.find('a').text
             url = news.find('a')['href']
             published = news.find('time').text
-            result_news.append({
-                "title": title,
-                "url": url,
-                "published": published
-            })
-        return result_news
-    return False
+            try:
+                published = datetime.strftime(published, '%Y-%m-%d')
+            except ValueError:
+                published = datetime.now()
+            save_news(title, url, published)
 
+
+
+def save_news(title, url, published):
+    news_exists = News.query.filter(News.url == url).count()
+    print(news_exists)
+    if not news_exists:
+        new_news = News(title=title, url=url, published=published)
+        db.session.add(new_news)
+        db.session.commit()
