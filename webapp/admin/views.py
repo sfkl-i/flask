dec53648a6ad165362296baab1c32409a0360322
sfkl-i <safa1320@gmail.com>
2020-06-29 23:15:52 +0300

    Переписал проект с использование Blueprints. Добалено меню и декоратор @admin_required. Создан базовый шаблон, переписаны остальные с помощью extends и include.

---
+++ webapp/admin/views.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+from webapp.user.decorators import admin_required
+
+blueprint = Blueprint('admin', __name__, url_prefix='/admin')
+
+@blueprint.route('/')
+@admin_required
+def admin_index():
+        page_title = "Панель управления" 
+        return render_template('admin/index.html', title=page_title)
---
+++ webapp/news/views.py
@@ -0,0 +1,12 @@
+from flask import render_template, Blueprint, current_app
+from webapp.weather import weather_by_city
+from webapp.news.models import News
+
+blueprint = Blueprint('news', __name__)
+
+@blueprint.route('/')
+def index():
+    page_title = "Новости Python"
+    weather = weather_by_city(current_app.config["WEATHER_DEFAULT_CITY"])
+    news_list = News.query.order_by(News.published.desc()).all()
+    return render_template("news/index.html", title=page_title, weather=weather, news_list=news_list)
