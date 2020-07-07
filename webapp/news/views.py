cddc746573f1eb775867351a482b776278931887
sfkl-i <safa1320@gmail.com>
2020-07-07 15:22:21 +0300

    Celery

--- webapp/news/views.py
+++ webapp/news/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, current_app
+from flask import abort, render_template, Blueprint, current_app
 from webapp.weather import weather_by_city
 from webapp.news.models import News
 
@@ -8,5 +8,14 @@ blueprint = Blueprint('news', __name__)
 def index():
     page_title = "Новости Python"
     weather = weather_by_city(current_app.config["WEATHER_DEFAULT_CITY"])
-    news_list = News.query.order_by(News.published.desc()).all()
+    news_list = News.query.filter(News.text.isnot(None)).order_by(News.published.desc()).all()
     return render_template("news/index.html", title=page_title, weather=weather, news_list=news_list)
+
+@blueprint.route('/news/<int:news_id>')
+def single_news(news_id):
+    my_news = News.query.filter(News.id==news_id).first()
+
+    if not my_news:
+        abort(404)
+
+    return render_template('news/single_news.html', title=my_news.title, news=my_news)
